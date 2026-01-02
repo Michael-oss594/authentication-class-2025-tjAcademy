dca29cec7d9dcabcd5f9968bd822fabd233d5592
Michael <oolayinka29@gmail.com>
2026-01-02 12:44:05 +0100

    authentication-class-2025-tjAcademy

--- src/config/db.js
+++ src/config/db.js
@@ -2,15 +2,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-const DB_URL = process.env.DB_URL || 'mongodb+srv://mikky:fOhG7nh13WSTliXw@cluster0.u1z3xfi.mongodb.net/myauthClass';
-
 const connectDB = async () => {
-    try {
-        await mongoose.connect(DB_URL); 
-        console.log('MongoDB connected successfully');
-    } catch (error) {
-        console.error('MongoDB connection failed:', error);
-        process.exit(1);
-    }
+
+    await mongoose.connect(process.env.DB_URL);
+    console.log("MongoDB connected successfully");
+    
 };
+
 module.exports = connectDB;
+
+
